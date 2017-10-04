dddf4c5693c815421f7a1b5d175d5d15a54701ce
saba <saba.jahanian@gmail.com>
2017-10-04 09:31:08 +0330

    change some styles

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -1,49 +1,50 @@
 var builder = new karmaBuilder();
-QUnit.test('karmaCreateModel',function (assert) {
+QUnit.test ( 'karmaCreateModel',function ( assert ) {
 
     var kamraModelResult = {
         1 : {
-            type : 'shortcode_test',
-            parentId : '0',
-            attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
+            'type' : 'shortcode_test',
+            'parent_id' : '0',
+            'attr' : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
 
         }
-    }
+    };
 
-    var model = {type : 'shortcode_test', parentId : '0', attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\''};
-    assert.deepEqual(builder.createShortcode(model),kamraModelResult);
+    var model = { type : 'shortcode_test', parent_id : '0', attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\'' };
+    assert.deepEqual ( builder.createShortcode ( model ) ,kamraModelResult );
 
 });
 
 //delete test
-QUnit.test("karmaDeleteModel",function (assert) {
+QUnit.test ( "karmaDeleteModel",function ( assert ) {
 
     var karmaDeleteResult = {
-        1 : {
-            type : 'shortcode_test',
-            parentId : '0',
-            attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
+		1 : {
+			'type' : 'shortcode_test',
+			'parent_id' : '0',
+			'attr' : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
         }
     };
     builder.karmaModel = {
         1 : {
-            type : 'shortcode_test',
-            parentId : '0',
-            attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
+            'type' : 'shortcode_test',
+            'parent_id' : '0',
+            'attr' : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
         },
        2 : {
-           type : 'shortcode_test',
-           parentId : '0',
-           attr : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
+           'type' : 'shortcode_test',
+           'parent_id' : '0',
+           'attr' : 'color:\'red\' font:\'arial\' bg:\'#000fff\''
         }
-    }
+    };
 
     assert.deepEqual( builder.deleteShortcode( 2 ),karmaDeleteResult );
+
 });
 
 
 QUnit.test( "karmaSaveContent", function ( assert ) {
 
 	assert.deepEqual( builder.saveContent(),true);
-
+    
 });
