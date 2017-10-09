d779fa2436a12b49cbea2563cbcf955a7b20307e
saba <saba.jahanian@gmail.com>
2017-10-09 13:50:29 +0330

    start section setting panel

--- public/templates/element-setting-panel-template.php
+++ public/templates/element-setting-panel-template.php
@@ -1,3 +1,17 @@
-<div id="karma-shortcode-setting-panel">
-<div class="name" > {{ data.name }} </div>
+<div id="karma-element-setting-panel-container">
+	<div class="karma-element-setting-panel">
+		<div class="karma-element-setting-panel-header">
+			<div class="karma-element-setting-panel-header-title">{{{ data.headerTitle }}}</div>
+			<div class="karma-element-setting-panel-header-svg"></div>
+		</div>
+		<div class="karma-element-setting-panel-content"></div>
+	</div>
+		<div class="karma-shortcode-setting-panel-extra">
+			<ul>
+				<li><span>Add to library</span></li>
+				<li><span>Delete</span></li>
+				<li><span>Duplicate</span></li>
+				<li><span>Help</span></li>
+			</ul>
+		</div>
 </div>
