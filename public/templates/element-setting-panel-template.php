fe3f8760f6c8242af9f8f6da9f1bc7882e5b0810
saba <saba.jahanian@gmail.com>
2017-10-16 12:08:01 +0330

    add svg to section panel

--- public/templates/element-setting-panel-template.php
+++ public/templates/element-setting-panel-template.php
@@ -4,22 +4,34 @@
 			<div class="karma-element-setting-panel-header-title">{{{ data.headerTitle }}}</div>
 			<div class="karma-element-setting-panel-header-svg">
 				<div class="revert-svg">
-					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-					 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-						<style type="text/css">
-							.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-						</style>
-						<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+					<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+						<title>Revert</title>
+						<desc>Created with Sketch.</desc>
+						<defs></defs>
+						<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+							<g id="Revert" stroke="#394959">
+								<g transform="translate(5.000000, 5.000000)">
+									<polyline id="Path-2-Copy-5" transform="translate(5.000000, 3.500000) rotate(90.000000) translate(-5.000000, -3.500000) " points="2 2 5.23724972 5 8 2"></polyline>
+									<path d="M7,13 C8.94764998,13 10.6352057,11.8864042 11.460687,10.2611926 C11.8055924,9.58214122 12,8.81377377 12,8 C12,7.02054523 11.7183728,6.106868 11.2316786,5.33552845 C10.3462125,3.93219347 8.78196897,3 7,3 C4.23857625,3 2,5.23857625 2,8" id="Oval-3" transform="translate(7.000000, 8.000000) scale(-1, 1) rotate(215.000000) translate(-7.000000, -8.000000) "></path>
+								</g>
+							</g>
+						</g>
 					</svg>
 				</div>
 
 				<div class="close-svg">
-					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-						 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-						<style type="text/css">
-							.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-						</style>
-						<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+					<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+						<title>Close</title>
+						<desc>Created with Sketch.</desc>
+						<defs></defs>
+						<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g id="Close" fill="#394959">
+								<g id="Group" transform="translate(12.192388, 12.192388) rotate(-315.000000) translate(-12.192388, -12.192388) translate(5.692388, 5.692388)">
+									<rect id="Rectangle-4" transform="translate(6.500000, 6.500000) rotate(90.000000) translate(-6.500000, -6.500000) " x="6" y="0" width="1" height="13" rx="0.5"></rect>
+									<rect id="Rectangle" x="6" y="0" width="1" height="13" rx="0.5"></rect>
+								</g>
+							</g>
+						</g>
 					</svg>
 				</div>
 
@@ -34,12 +46,23 @@
 			<ul>
 				<li class="svg-extra-add">
 					<div class="svg-extra ">
-						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-									<style type="text/css">
-										.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-									</style>
-							<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+						<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<title>Add to library</title>
+							<desc>Created with Sketch.</desc>
+							<defs></defs>
+							<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+								<g id="Add-to-library" stroke="#394959">
+									<g id="Group-7" transform="translate(6.000000, 5.000000)">
+										<g id="Group">
+											<polyline id="Path-2" stroke-linejoin="round" points="0.842285156 8.21422649 0.842285156 13.2142265 11.8422852 13.2142265 11.8422852 8.21422649"></polyline>
+											<g id="Group-6" transform="translate(3.000000, 0.000000)">
+												<polyline id="Path-4" stroke-linejoin="round" transform="translate(3.376990, 4.859834) rotate(-45.000000) translate(-3.376990, -4.859834) " points="1.08273073 2.58857609 1.08273073 7.13109262 5.67124959 7.13109262"></polyline>
+												<path d="M3.37262119,7.91068318 L3.37262119,0.734894772" id="Path-5"></path>
+											</g>
+										</g>
+									</g>
+								</g>
+							</g>
 						</svg>
 					</div>
 					<span>Add to library</span>
@@ -47,12 +70,22 @@
 
 				<li>
 					<div class="svg-extra">
-						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-									<style type="text/css">
-										.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-									</style>
-							<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+						<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<title>Trash</title>
+							<desc>Created with Sketch.</desc>
+							<defs></defs>
+							<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<g id="Trash">
+									<g id="Group" transform="translate(4.000000, 4.000000)">
+										<rect id="Rectangle-5" stroke="#394959" x="2.48828125" y="2.5" width="9.69958496" height="12.1006944" rx="1"></rect>
+										<rect id="Rectangle-9" fill="#394959" x="0.405395508" y="2" width="13.649292" height="1" rx="0.5"></rect>
+										<path d="M5.48828125,2.5 L9.48828125,2.5 L9.48828125,1 C9.48828125,0.723857625 9.26442362,0.5 8.98828125,0.5 L5.98828125,0.5 C5.71213888,0.5 5.48828125,0.723857625 5.48828125,1 L5.48828125,2.5 Z" id="Rectangle-10" stroke="#394959"></path>
+										<rect id="Rectangle-11" fill="#394959" x="4.98828125" y="6" width="1" height="5" rx="0.5"></rect>
+										<rect id="Rectangle-11-Copy" fill="#394959" x="8.98828125" y="6" width="1" height="5" rx="0.5"></rect>
+										<rect id="Rectangle-11-Copy-2" fill="#394959" x="6.98828125" y="6" width="1" height="5" rx="0.5"></rect>
+									</g>
+								</g>
+							</g>
 						</svg>
 					</div>
 					<span>Delete</span>
@@ -60,12 +93,18 @@
 
 				<li>
 					<div class="svg-extra">
-						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-									<style type="text/css">
-										.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-									</style>
-							<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+						<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<title>Duplicate</title>
+							<desc>Created with Sketch.</desc>
+							<defs></defs>
+							<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+								<g id="Duplicate" stroke="#3D4D5D" fill="#FFFFFF">
+									<g id="Group" transform="translate(6.000000, 6.000000)">
+										<rect id="Rectangle-4-Copy-19" x="0" y="3" width="8.01589749" height="8.01589749" rx="1"></rect>
+										<rect id="Rectangle-4-Copy-20" x="3" y="0" width="8.01589749" height="8.01589749" rx="1"></rect>
+									</g>
+								</g>
+							</g>
 						</svg>
 					</div>
 					<span>Duplicate</span>
@@ -73,12 +112,16 @@
 
 				<li>
 					<div class="svg-extra">
-						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							 viewBox="0 0 10 10" style="enable-background:new 0 0 10 10;" xml:space="preserve">
-									<style type="text/css">
-										.st0{fill:none;stroke:#000000;stroke-miterlimit:10;}
-									</style>
-							<path class="st0" d="M1.2,5.6l2.5,2.6L9,1.7"/>
+						<svg width="25px" height="25px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<title>Help</title>
+							<desc>Created with Sketch.</desc>
+							<defs></defs>
+							<g id="SVG-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<g id="Help">
+									<path d="M10,9.5 C10.2381185,7.83333333 11.0714518,7 12.5,7 C14.6428223,7 15,9 14.5,10 C14,11 12,12 12,13.5 C12,14.5 12,15.1666667 12,15.5" id="Path-6" stroke="#394959"></path>
+									<circle id="Oval-2" fill="#394959" cx="12" cy="18" r="1"></circle>
+								</g>
+							</g>
 						</svg>
 					</div>
 					<span>Help</span>
