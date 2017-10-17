3914d47831a842cc389223d27c0141fbb7cf8f36
shahinnejad <iman.shahinnezhad68@gmail.com>
2017-10-17 12:52:52 +0330

    finish functinality of group

--- public/templates/extends/setting-panel-groups/script.js
+++ public/templates/extends/setting-panel-groups/script.js
@@ -2,13 +2,8 @@ jQuery('body').off( 'karma_finish_form_builder.groups' ).on( 'karma_finish_form_
 	var $ = jQuery;
 	$( '.karma-setting-panel-group-button' ).click( function () {
 		$(this).next(".karma-group-panel" ).slideToggle();
-		if($(".karma-group-panel" ).css('display') == 'block'){
 
-			$('.karma-group-button-shape').addClass('karma-group-button-shape-rotate');
-		}
-		else{
-		
+			$(this).find('.karma-group-button-shape').toggleClass('karma-group-button-shape-rotate');
 
-		}
 	} )
 } );
