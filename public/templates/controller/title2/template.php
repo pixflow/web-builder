820830a1b8f3b7a0c3ba8681b2e74cfc616236b7
shahinnejad <iman.shahinnezhad68@gmail.com>
2017-10-23 13:42:44 +0330

    title shortcode setting panel refine

--- public/templates/controller/title2/template.php
+++ public/templates/controller/title2/template.php
@@ -1,11 +1,11 @@
 
 <div class="karma-title2-controller">
 	<div class="karma-title2-field"> {{{data.value}}} </div>
-</div>
-<# if( data.seprator != "" ) { #>
-	<# if( data.seprator == "full" ){ #>
-		<hr class="karma-full-seprator">
-		<#}else{ #>
-		<hr class="karma-container-seprator">
+	<# if( data.seprator != "" ) { #>
+		<# if( data.seprator == "full" ){ #>
+			<hr class="karma-full-seprator">
+			<#}else{ #>
+				<hr class="karma-container-seprator">
+		<# } #>
 	<# } #>
-<# } #>
+</div>
