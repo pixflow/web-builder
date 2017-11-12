ce22359703ba38e92f2ce097e9b98fd5e35b1d44
shahinnejad <iman.shahinnezhad68@gmail.com>
2017-11-12 09:39:26 +0330

    refine header hover in builder

---
+++ builder/templates/controller/switch-panel/template.php
@@ -0,0 +1,7 @@
+<div class="karma-switch-panel-container">
+	<button class="karma-switch-panel-button" data-action="{{ data.action }}" data-form="{{ data.form }}">
+		<div class="karma-switch-panel-shape"></div>
+		<p>{{ data.text }}</p>
+	</button>
+	<div id="new-b-panel"></div>
+</div>
