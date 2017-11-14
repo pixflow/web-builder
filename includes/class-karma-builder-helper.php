eaaf0aa9d98a635afa6507fe7fd4ec30cee29ab1
mojde-pajang <mojde.pajang@gmail.com>
2017-11-14 08:27:39 +0330

    refine load svg function

--- includes/class-karma-builder-helper.php
+++ includes/class-karma-builder-helper.php
@@ -1,7 +1,17 @@
 <?php
+
+/**
+ * Returns content of file (use for svg tag )
+ *  @param    string $url the url of svg file
+ *  @since    1.0.0
+ *  @access   public
+ * @return    {string} html of svg file
+ */
 function karma_load_svg( $url ){
 
-	  $response = wp_remote_get($url);
-	 return $response['body'];
-
+	$response = wp_remote_get( $url );
+	if( is_wp_error( $response ) ){
+		return $response[ 'body' ];
+	}
+	return '';
 }
