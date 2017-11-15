aec36ebdbe3b1e66389d3d16f29771ff9c2c6f1c
shahinnejad <iman.shahinnezhad68@gmail.com>
2017-11-15 11:04:38 +0330

    change for create minify

--- builder/js/karma-builder-actions.js
+++ builder/js/karma-builder-actions.js
@@ -1,10 +1,11 @@
 (function () {
 
 	/**
-	 *  On clicking in builder-publish button an event trigger on karma view
+	 *  On clicking in builder-publish button an event trigger on karma vieww
 	 */
 	document.getElementsByClassName( 'builder-publish' )[0].addEventListener("click", function(){
 
+		frames['karma-builder-iframe'].contentWindow.window.KarmaView.$el.trigger('karma_builder_published');
 		frames['karma-builder-iframe'].contentWindow.window.KarmaView.$el.trigger('karma_builder_published');
 
 	});
