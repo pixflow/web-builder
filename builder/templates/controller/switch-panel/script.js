7871e1f8ed9ed3a75bd0edf0b8398e79355f7d65
Mohammad Rezaei <me@rezaei.click>
2017-11-21 11:01:52 +0330

    Fix open media library in firefox ( Ermia )

--- builder/templates/controller/switch-panel/script.js
+++ builder/templates/controller/switch-panel/script.js
@@ -1,20 +1,23 @@
 jQuery( document ).off( 'karma_finish_form_builder.switch-panel-extra' ).on( 'karma_finish_form_builder.switch-panel-extra', function( e, view ) {
 
+	if( null == document.querySelector('#new-form-builder-panel') ){
+		return;
+	}
+
 	var switchPanels = document.querySelectorAll( ".karma-switch-panel-container" ),
 			settingPanel = document.querySelector('#karma-Builder-form'),
 			settingPanelHeight = settingPanel.getAttribute('data-height') ,
-			newFormBuilderPanel = document.querySelector('#new-form-builder-panel'),
 			$ = jQuery;
 
-	if( null == newFormBuilderPanel ){
-		return;
-	}
+
 	var panelHeight = document.querySelector('#new-form-builder-panel').getAttribute('data-height');
 
 	for( var switchPanel in switchPanels ) {
-		if ('length' === switchPanel) {
+
+		if ( 'length' === switchPanel || "object" != typeof switchPanels [ switchPanel ] ) {
 			break;
 		}
+
 		switchPanels[ switchPanel ].querySelector('.karma-switch-panel-button').addEventListener( 'click', function () {
 
 			var action = this.getAttribute('data-action');
