097763f6c19c23aacdf8529eb323611346f79756
shahinnejad <iman.shahinnezhad68@gmail.com>
2017-11-27 13:25:11 +0330

    functionality of close panel

--- builder/js/element-panel.js
+++ builder/js/element-panel.js
@@ -1,4 +1,4 @@
-( function( $, karmaBuilder){
+( function( $, karmaBuilder ){
 
 	karmaBuilder.elementPanel = Backbone.View.extend({
 
@@ -8,7 +8,9 @@
 		 * @since   1.0.0
 		 */
 		events : {
+
 			"click .element-panel-add-element-button"		: "openAddElementView",
+			"click "										: "stopClickInPanel",
 
 		},
 
@@ -21,11 +23,24 @@
 		initialize: function() {
 
 			this.setElement( $( '<div class="karma-element-panel-container">' ) );
+			this.render();
 			this.createAddElementPanel();
 			this.createTemplatesPanel();
 			this.createUnsplashPanel();
+			this.closeElementPanel();
 
-			this.render();
+
+		},
+
+		/**
+		 * stop close panel when click in panel
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
+		stopClickInPanel : function (e) {
+
+			e.stopPropagation();
 
 		},
 
@@ -41,43 +56,79 @@
 
 		},
 
+		/**
+		 * create add element panel with import template
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
 		createAddElementPanel: function(){
 
-			var template = '<div>' + KarmaView.getWpTemplate('karma-element-panel-add-element', {} ) + '</div>';
+			var template = '<div>' + KarmaView.getWpTemplate( 'karma-element-panel-add-element', {} ) + '</div>';
 			this.el.appendChild( $( template )[0] );
 			$( document ).trigger( 'karma_finish_element_panel_html', [ this ] );
 
 		},
 
+		/**
+		 * create Templates panel with import template
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
 		createTemplatesPanel: function(){
 
-			var template = '<div>' + KarmaView.getWpTemplate('karma-element-panel-templates', {} ) + '</div>';
-
+			var template = '<div>' + KarmaView.getWpTemplate( 'karma-element-panel-templates', {} ) + '</div>';
 			this.el.appendChild( $( template )[0] )
 			$( document ).trigger( 'karma_finish_element_panel_html', [ this ] );
 
 		},
 
+		/**
+		 * create Unsplash with import template
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
 		createUnsplashPanel: function(){
 
-			var template = '<div>' + KarmaView.getWpTemplate('karma-element-panel-unsplash', {} ) + '</div>';
-
+			var template = '<div>' + KarmaView.getWpTemplate( 'karma-element-panel-unsplash', {} ) + '</div>';
 			this.el.appendChild( $( template )[0] );
 			$( document ).trigger( 'karma_finish_element_panel_html', [ this ] );
 
 		},
 
+		/**
+		 * open add element panel
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
 		openAddElementView: function () {
 
 			var addElement = document.querySelector( '.karma-element-panel-add-element-view' );
 			if( null != addElement ){
-
-				addElement.classList.add("element-panel-show");
-
+				addElement.classList.add( "element-panel-show" );
 			}
 
 		},
 
+		/**
+		 * close add element panel
+		 *
+		 * @since   1.0.0
+		 * @returns {void}
+		 */
+		closeElementPanel: function() {
+
+			$( document ).on( 'click', function(){
+				var addElement = document.querySelector( '.karma-element-panel-add-element-view' );
+				if( null != addElement ) {
+					addElement.classList.remove("element-panel-show");
+				}
+			});
+
+		},
 
 	});
 
