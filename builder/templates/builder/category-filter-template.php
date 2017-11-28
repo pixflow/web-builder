75a98baf9cc9ae29ac591bad352e6583df8a74cd
saba <saba.jahanian@gmail.com>
2017-11-28 09:46:09 +0330

    category filter style

--- builder/templates/builder/category-filter-template.php
+++ builder/templates/builder/category-filter-template.php
@@ -4,42 +4,43 @@
     <ul>
         <li class="karma-addcontent">
             <div class="karma-addcontent-icon">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-1.svg' ); ?>
+                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-elements.svg' ); ?>
             </div>
             <div class="karma-addcontent-name">Elements</div
         </li>
 
         <li class="karma-addcontent">
             <div class="karma-addcontent-icon">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-2.svg' ); ?>
+                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-photos.svg' ); ?>
             </div>
             <div class="karma-addcontent-name">Photos</div>
         </li>
 
-        <li class="karma-addcontent">
-            <div class="karma-addcontent-icon karma-addcontent-disable">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-3.svg' ); ?>
-            </div>
-            <div class="karma-addcontent-name">Sections</div>
+
+        <li class="karma-addcontent karma-addcontent-disable">
+			<div class="karma-addcontent-icon">
+				<?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-sections.svg' ); ?>
+			</div>
+			<div class="karma-addcontent-name">Sections</div>
         </li>
 
-        <li class="karma-addcontent">
+        <li class="karma-addcontent karma-addcontent-disable">
             <div class="karma-addcontent-icon">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-4.svg' ); ?>
+                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-header.svg' ); ?>
             </div>
             <div class="karma-addcontent-name">Header</div>
         </li>
 
-        <li class="karma-addcontent">
+        <li class="karma-addcontent karma-addcontent-disable">
             <div class="karma-addcontent-icon">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-5.svg' ); ?>
+                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-footer.svg' ); ?>
             </div>
             <div class="karma-addcontent-name">Footer</div>
         </li>
 
-        <li class="karma-addcontent">
+        <li class="karma-addcontent karma-addcontent-disable">
             <div class="karma-addcontent-icon">
-                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-icon-6.svg' ); ?>
+                <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-shop.svg' ); ?>
             </div>
             <div class="karma-addcontent-name">Shop</div>
         </li>
