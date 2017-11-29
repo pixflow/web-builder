3c7134ea1e962ba61fc45e8eca6544540161b6af
Mohammad Rezaei <me@rezaei.click>
2017-11-29 16:15:56 +0330

    refine sass

--- builder/templates/builder/element-panel-top-header-template.php
+++ builder/templates/builder/element-panel-top-header-template.php
@@ -2,5 +2,12 @@
 	<input type="search" class="search-text" placeholder="Search...">
 	<div class="karma-builder-element-panel-gather-menu">
 		<?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/gather-menu.svg' ); ?>
+		<ul>
+			<li>All Categories</li>
+			<li>Agency</li>
+			<li>Portfolio</li>
+			<li>Creative</li>
+			<li>Shop</li>
+		</ul>
 	</div>
 </div>
