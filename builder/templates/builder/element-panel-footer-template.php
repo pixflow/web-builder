66e876e351e9f58e969cc0a9709285b89fc54326
saba <saba.jahanian@gmail.com>
2017-12-03 12:47:57 +0330

    edite element panel style

--- builder/templates/builder/element-panel-footer-template.php
+++ builder/templates/builder/element-panel-footer-template.php
@@ -1,6 +1,6 @@
 <div class="element-panel-section-container element-panel-footer element-panel-deactive-part" >
 	<div class="element-panel-part-img"> <img  src="<?php  echo KARMA_BUILDER_URL . 'builder/media/karma-premium.png' ; ?>"></div>
-	<div class="element-panel-part-title"><?php echo esc_attr__("Karma Premium", 'karma')?></div>
+	<div class="element-panel-part-title"><?php echo esc_attr__("Footer Premium", 'karma')?></div>
 	<div class="element-panel-part-description"> All you need to do your best work, together in one package that works seamlessly from your computer, phone or tablet.
 	</div>
 	<div class="element-panel-part-button"><?php echo esc_attr__("Subscribe", 'karma')?></div>
