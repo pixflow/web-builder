7bca92cb2a1716d922e15628ed711835f38d0371
mojde-pajang <mojde.pajang@gmail.com>
2017-12-03 08:16:36 +0330

    start of filterizing

--- builder/templates/builder/element-panel-top-header-template.php
+++ builder/templates/builder/element-panel-top-header-template.php
@@ -4,11 +4,11 @@
 	<div class="karma-builder-element-panel-gather-menu">
 		<?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/gather-menu.svg' ); ?>
 		<ul>
-			<li>All Categories</li>
-			<li>Agency</li>
-			<li>Portfolio</li>
-			<li>Creative</li>
-			<li>Shop</li>
+			<li data-filter="all">All Categories</li>
+			<li data-filter="agency">Agency</li>
+			<li data-filter="portfolio">Portfolio</li>
+			<li data-filter="creative">Creative</li>
+			<li data-filter="shop">Shop</li>
 		</ul>
 	</div>
 </div>
