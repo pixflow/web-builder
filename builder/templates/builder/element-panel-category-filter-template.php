40bb9719914aa192631660f91ace6d5b50956d7a
saba <saba.jahanian@gmail.com>
2017-12-04 08:30:29 +0330

    correct php code

--- builder/templates/builder/element-panel-category-filter-template.php
+++ builder/templates/builder/element-panel-category-filter-template.php
@@ -1,18 +1,18 @@
 <div class="karma-builder-addcontent">
-    <div class="karma-builder-addcontent-title"><?php echo esc_attr__("Add Content", 'Karma')?></div>
+    <div class="karma-builder-addcontent-title"><?php echo esc_attr__("Add Content", 'Karma'); ?></div>
     <ul>
         <li class="karma-addcontent karma-addcontent-active" data-tab="karma-element-panel-list" >
             <div class="karma-addcontent-icon">
                 <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-elements.svg' ); ?>
             </div>
-            <div class="karma-addcontent-name"><?php echo esc_attr__("Elements", 'Karma')?></div
+            <div class="karma-addcontent-name"><?php echo esc_attr__("Elements", 'Karma'); ?></div
         </li>
 
         <li class="karma-addcontent" data-tab="element-panel-image">
             <div class="karma-addcontent-icon">
                 <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-photos.svg' ); ?>
             </div>
-            <div class="karma-addcontent-name"><?php echo esc_attr__("Photos", 'Karma')?></div>
+            <div class="karma-addcontent-name"><?php echo esc_attr__("Photos", 'Karma'); ?></div>
         </li>
 
 
@@ -20,28 +20,28 @@
 			<div class="karma-addcontent-icon">
 				<?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-sections.svg' ); ?>
 			</div>
-			<div class="karma-addcontent-name"><?php echo esc_attr__("Sections", 'Karma')?></div>
+			<div class="karma-addcontent-name"><?php echo esc_attr__("Sections", 'Karma'); ?></div>
         </li>
 
         <li class="karma-addcontent karma-addcontent-disable" data-tab="element-panel-header">
             <div class="karma-addcontent-icon">
                 <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-header.svg' ); ?>
             </div>
-            <div class="karma-addcontent-name"><?php echo esc_attr__("Header", 'Karma')?></div>
+            <div class="karma-addcontent-name"><?php echo esc_attr__("Header", 'Karma'); ?></div>
         </li>
 
         <li class="karma-addcontent karma-addcontent-disable" data-tab="element-panel-footer">
             <div class="karma-addcontent-icon">
                 <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-footer.svg' ); ?>
             </div>
-            <div class="karma-addcontent-name"><?php echo esc_attr__("Footer", 'Karma')?></div>
+            <div class="karma-addcontent-name"><?php echo esc_attr__("Footer", 'Karma'); ?></div>
         </li>
 
         <li class="karma-addcontent karma-addcontent-disable" data-tab="element-panel-shop">
             <div class="karma-addcontent-icon">
                 <?php print karma_load_svg( KARMA_BUILDER_URL . 'builder/media/svg/add-panel-shop.svg' ); ?>
             </div>
-            <div class="karma-addcontent-name"><?php echo esc_attr__("Shop", 'Karma')?></div>
+            <div class="karma-addcontent-name"><?php echo esc_attr__("Shop", 'Karma'); ?></div>
         </li>
     </ul>
 </div>
