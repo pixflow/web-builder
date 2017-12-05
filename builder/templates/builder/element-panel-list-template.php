5a71922aed8f7592f0e308499ee6bec3a4854381
mojde-pajang <mojde.pajang@gmail.com>
2017-12-05 10:51:36 +0330

    refine height of header section

--- builder/templates/builder/element-panel-list-template.php
+++ builder/templates/builder/element-panel-list-template.php
@@ -4,7 +4,7 @@
 			<# _.each( data.elementInfo, function( element ){
 		if( false !== element.showInList ){
 		#>
-		<div class="karma-element-single-element agency mysection" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+			<div class="karma-element-single-element agency mysection" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
 			<div class="karma-element-single-element-icon">
 				<# print( element.icon ); #>
 			</div>
@@ -14,47 +14,48 @@
 				</span>
 			</div>
 		</div>
-				<div class="karma-element-single-element agency mysection" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
-					<div class="karma-element-single-element-icon">
-						<# print( element.icon ); #>
-					</div>
-					<div class="karma-element-single-element-name" >
-				<span>
-					<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
-				</span>
-					</div>
+			<div class="karma-element-single-element agency mysection" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+				<div class="karma-element-single-element-icon">
+					<# print( element.icon ); #>
 				</div>
-				<div class="karma-element-single-element portfolio premium" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
-					<div class="karma-element-single-element-icon">
-						<# print( element.icon ); #>
-					</div>
-					<div class="karma-element-single-element-name" >
-				<span>
-					<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
-				</span>
-					</div>
+				<div class="karma-element-single-element-name" >
+			<span>
+				<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
+			</span>
 				</div>
-				<div class="karma-element-single-element shop free" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
-					<div class="karma-element-single-element-icon">
-						<# print( element.icon ); #>
-					</div>
-					<div class="karma-element-single-element-name" >
-				<span>
-					<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
-				</span>
-					</div>
+			</div>
+			<div class="karma-element-single-element portfolio premium" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+				<div class="karma-element-single-element-icon">
+					<# print( element.icon ); #>
 				</div>
-				<div class="karma-element-single-element portfolio premium" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
-					<div class="karma-element-single-element-icon">
-						<# print( element.icon ); #>
-					</div>
-					<div class="karma-element-single-element-name" >
-				<span>
-					<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
-				</span>
-					</div>
+				<div class="karma-element-single-element-name" >
+			<span>
+				<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
+			</span>
 				</div>
-				<div class="karma-element-single-element creative free" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+			</div>
+			<div class="karma-element-single-element shop free" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+				<div class="karma-element-single-element-icon">
+					<# print( element.icon ); #>
+				</div>
+				<div class="karma-element-single-element-name" >
+			<span>
+				<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
+			</span>
+				</div>
+			</div>
+			<div class="karma-element-single-element portfolio premium" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
+
+				<div class="karma-element-single-element-icon">
+					<# print( element.icon ); #>
+				</div>
+				<div class="karma-element-single-element-name" >
+			<span>
+				<# print( element.elementName.replace( 'karma_', '', element.elementName ) ); #>
+			</span>
+				</div>
+			</div>
+			<div class="karma-element-single-element creative free" data-category="<# print( element.category.join(',') ); #>" data-element-name="{{ element.elementName }}" >
 					<div class="karma-element-single-element-icon">
 						<# print( element.icon ); #>
 					</div>
