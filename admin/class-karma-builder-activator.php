dfb242b0e170b2582d7a3bc1d0d90bfef66951c0
Ermia <s.qasemi92.itsu@gmail.com>
2018-01-24 09:29:39 +0330

    unchanged file

---
+++ admin/class-karma-builder-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://pixflow.net
+ * @since      1.0.0
+ *
+ * @package    Karma_Builder
+ * @subpackage Karma_Builder/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Karma_Builder
+ * @subpackage Karma_Builder/includes
+ * @author     Pixflow <info@pixflow.net>
+ */
+class Karma_Builder_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
