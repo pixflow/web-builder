24267fcb943c69d641d2701bfdd601a00be96e35
saba <saba.jahanian@gmail.com>
2018-02-25 12:04:46 +0330

    add hover for sections in drag

--- builder/templates/builder/element-panel-blocks-template.php
+++ builder/templates/builder/element-panel-blocks-template.php
@@ -1,5 +1,6 @@
 <# var i = 0; _.each( data.blocks, function( block ){;#>
 <div data-block-id="{{ i++ }}" class="karma-section-element" >
-	<img class="karma-section-image" src="{{ block.image }}" width="240" height="{{ block.height }}">
+	<div class="karma-section-hover"></div>
+		<img class="karma-section-image" src="{{ block.image }}" width="240" height="{{ block.height }}">
 </div>
 <# }); #>
